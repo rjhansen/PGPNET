c7aae9499aa9ba30eab6bff07c91d9683c35a4ee
Robert J. Hansen <rjh@sixdemonbag.org>
2017-08-18 19:53:23 -0400

    Initial checkin

---
+++ csharp/PGPNET Setup/PGPNET Setup/Controller.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows;
+using System.Threading.Tasks;
+
+namespace PGPNET_Setup
+{
+    static class Controller
+    {
+        static string gnupgPath = String.Empty;
+        static string prrPath = String.Empty;
+        public static string GnuPGPath
+        {
+            get
+            {
+                if (String.Empty != gnupgPath) return gnupgPath;
+
+                try
+                {
+                    using (var registryKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"Software\GnuPG"))
+                    {
+                        gnupgPath = (String)registryKey.GetValue("Install Directory") + @"\bin\gpg.exe";
+                        if (File.Exists(gnupgPath)) return gnupgPath;
+                    }
+                }
+                catch
+                {
+                }
+                MessageBox.Show("GnuPG could not be found.  Please check your installation.", "GnuPG Not Found", MessageBoxButton.OK);
+                Application.Current.Shutdown();
+                return null;
+            }
+        }
+
+        public static string PRRPath
+        {
+            get
+            {
+                if (String.Empty != prrPath) return prrPath;
+
+                var profileDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Thunderbird", "Profiles");
+
+                if (!Directory.Exists(profileDir))
+                {
+                    MessageBox.Show("Could not find Thunderbird profile folder.", "File not found", MessageBoxButton.OK);
+                    Application.Current.Shutdown();
+                }
+
+                string[] subdirs = (from dirname in Directory.GetDirectories(profileDir)
+                                    where dirname.EndsWith(".default", StringComparison.CurrentCulture)
+                                    select dirname).ToArray();
+                if (subdirs.Length == 0)
+                {
+                    MessageBox.Show("Could not find any profiles in your Thunderbird folder.", "Profile not found", MessageBoxButton.OK);
+                    Application.Current.Shutdown();
+                }
+                if (subdirs.Length > 1)
+                {
+                    MessageBox.Show("Multiple profiles found.  This application can't handle that — yet.", "Too many profiles", MessageBoxButton.OK);
+                    Application.Current.Shutdown();
+                }
+
+                profileDir = subdirs[0];
+
+                if (!File.Exists(Path.Combine(profileDir, "pgprules.xml")))
+                {
+                    MessageBox.Show("No Enigmail per-recipient rules in your profile.", "No pgprules.xml", MessageBoxButton.OK);
+                    Application.Current.Shutdown();
+                }
+                prrPath = Path.Combine(profileDir, "pgprules.xml");
+                return prrPath;
+            }
+        }
+
+        public static void AdjustGPGConf()
+        {
+            var gpgconf = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "GnuPG", "gpg.conf");
+            List<String> lines = null;
+            try
+            {
+                using (var fh = File.OpenText(gpgconf))
+                {
+                    lines = (from line in fh.ReadToEnd().Split('\n')
+                             where (!line.Trim().StartsWith("personal-") && line.Trim().Count() > 0)
+                             select line.Trim()).ToList();
+                }
+
+                lines.Add("personal-cipher-preferences AES256 CAMELLIA256 TWOFISH AES192 CAMELLIA192 AES CAMELLIA128 CAST5");
+                lines.Add("personal-digest-preferences SHA512 SHA384 SHA256 SHA224 RIPEMD160");
+                lines.Add("personal-compress-preferences BZIP2 ZIP ZLIB");
+
+                using (var fh = File.CreateText(gpgconf))
+                {
+                    foreach (var line in lines)
+                        fh.WriteLine(line);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Could not configure your GnuPG installation", "Error", MessageBoxButton.OK);
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ csharp/PGPNET Setup/PGPNET Setup/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PGPNET_Setup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        async private Task<string> DownloadKeys()
+        {
+            var url = "https://www.dropbox.com/s/bhpqzxcht527ruu/PGPNET.asc?dl=1";
+            var wc = new System.Net.WebClient();
+            try
+            {
+                var data = await wc.DownloadDataTaskAsync(url);
+                var foo = Encoding.UTF8.GetString(data);
+                return foo;
+            }
+            catch
+            {
+                MessageBox.Show("Couldn't download PGPNET files.", "Network error", MessageBoxButton.OK);
+                this.Close();
+                return null;
+            }
+        }
+
+        async private Task<string[]> DownloadIDs()
+        {
+            var url = "https://www.dropbox.com/s/j754hlknhot9sk8/Group%20Line.txt?dl=1";
+            var wc = new System.Net.WebClient();
+            try
+            {
+                var data = await wc.DownloadDataTaskAsync(url);
+                var rows = (from d in Encoding.UTF8.GetString(data).Split('\n')
+                            where d.StartsWith("group pgpnet@yahoogroups.com=")
+                            select d).ToArray();
+                if (rows.Length != 1)
+                {
+                    Console.Error.WriteLine("Corrupt membership file");
+                    Environment.Exit(1);
+                }
+                Console.Out.WriteLine("done.");
+                var ids = rows[0].Split('=')[1].Trim().Split(' ');
+
+                var rx = new Regex("^0x[A-F0-9]{16}$", RegexOptions.IgnoreCase);
+                return (from keyid in ids
+                        where rx.IsMatch(keyid)
+                        select keyid).ToArray();
+            }
+            catch
+            {
+                MessageBox.Show("Couldn't download PGPNET files.", "Network error", MessageBoxButton.OK);
+                this.Close();
+                return null;
+            }
+        }
+
+        private void ImportKeys(string keys)
+        {
+            using (var proc = new System.Diagnostics.Process()
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo()
+                {
+                    FileName = Controller.GnuPGPath,
+                    Arguments = "--no-tty --batch --quiet --import",
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true
+                }
+            })
+            {
+                proc.Start();
+                proc.StandardInput.Write(keys);
+                proc.StandardInput.Close();
+                proc.WaitForExit();
+            }
+        }
+
+        private void UpdatePGPRules(string[] ids)
+        {
+            XDocument xdoc = XDocument.Load(Controller.PRRPath);
+            var newdoc = new XElement("pgpRuleList",
+                                      from pgprule in xdoc.Descendants("pgpRule")
+                                      where pgprule.Attribute("email").Value != "{pgpnet@yahoogroups.com}"
+                                      select pgprule);
+            var newrule = new XElement("pgpRule");
+            newrule.SetAttributeValue("email", "{pgpnet@yahoogroups.com}");
+            newrule.SetAttributeValue("encrypt", 2);
+            newrule.SetAttributeValue("sign", 2);
+            newrule.SetAttributeValue("negateRule", 0);
+            newrule.SetAttributeValue("pgpMime", 2);
+            newrule.SetAttributeValue("keyId", String.Join(", ", ids));
+            newdoc.AddFirst(newrule);
+
+            using (var output = System.IO.File.CreateText(Controller.PRRPath))
+            {
+                output.Write(newdoc.ToString());
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            actionButton.Focus();
+        }
+
+        private void quitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        async private void actionButton_Click(object sender, RoutedEventArgs e)
+        {
+            quitButton.IsEnabled = false;
+            actionButton.IsEnabled = false;
+            var gpgPath = Controller.GnuPGPath;
+            findingGnuPG.Value = 1;
+            var prrPath = Controller.PRRPath;
+            findingEnigmail.Value = 1;
+            Controller.AdjustGPGConf();
+            configuringGnuPG.Value = 1;
+            acquiringData.IsIndeterminate = true;
+            var ids = await DownloadIDs();
+            var keys = await DownloadKeys();
+            acquiringData.IsIndeterminate = false;
+            acquiringData.Value = 1;
+            ImportKeys(keys);
+            UpdatePGPRules(ids);
+            configuringEnigmail.Value = 1;
+            quitButton.IsEnabled = true;
+        }
+
+        private void fileQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void helpPGPNET_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void helpAbout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
