9c5c1a2588060316bc1da9f7a4bd1cd4d6621118
Robert J. Hansen <rjh@sixdemonbag.org>
2017-08-18 21:57:35 -0400

    Wrote some user documentation.

--- csharp/PGPNET Setup/MakeMSI.ps1
+++ csharp/PGPNET Setup/MakeMSI.ps1
@@ -1,3 +1,26 @@
+# This code depends on PowerShell being set with the Microsoft
+# Visual Studio development environment variables, and with the
+# Wix toolset's bin directory being in your PATH.
+#
+# Unless you have an Authenticode signing certificate, comment
+# out the signtool lines.
+#
+# In your Documents\WindowsPowershell dir, add this to the file
+# profile.ps1.  (Note: you may need to alter the directory in the
+# first line to reflect your particular VS version.)
+#
+# =====
+#
+# pushd 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC'
+# cmd /c "vcvarsall.bat amd64&set" |
+# foreach {
+#   if ($_ -match "=") {
+#     $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+#   }
+# }
+# popd
+# write-host "`nVisual Studio 2015 Command Prompt variables set." -ForegroundColor Yellow
+
 & MSBuild.exe ".\PGPNET Setup.sln" /t:Clean /p:Configuration=Release /p:Platform="Any CPU"
 & MSBuild.exe ".\PGPNET Setup.sln" /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU"
 & signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a ".\PGPNET Setup\bin\Release\PGPNET Setup.exe"
